5391735eb1f607b5e17dfd2989426ac0873ed628
haidee09 <haideeram97@gmail.com>
2018-02-09 12:57:07 -0600

    add routes

--- API/controllers/device.js
+++ API/controllers/device.js
@@ -5,7 +5,7 @@ var crate = require('node-crate');
 var entidad;
 
 
-exports.read_devicesByDate= function(req, res) {
+exports.read_devicesByDate = function(req, res) {
     let date = req.params.date;
     console.log(date)
     crate.execute("select entity_id,location from etdevice where category=? and date_format(time_index) like ? group by entity_id",['smartphone',date+'%'])
@@ -26,7 +26,7 @@ exports.read_devicesByDate= function(req, res) {
     })
 };
 
-exports.read_deviceByIdDevice= function(req, res) {
+exports.read_deviceByIdDevice = function(req, res) {
     let idDevice = req.query['idDevice'];
     let date = req.query['date'];
 
@@ -70,7 +70,7 @@ exports.read_deviceByOwner = function(req, res) {
     })
 };
 
-exports.read_deviceByDateTime= function(req, res) {
+exports.read_deviceByOwnerDateTime= function(req, res) {
     let owner = req.query['owner'];
     let date = req.query['date'];
     let time = req.query['time'];
@@ -87,6 +87,29 @@ exports.read_deviceByDateTime= function(req, res) {
         console.log(dateFormated);
         entidad[0]['time_index'] = dateFormated;
         res.status(200).json(entidad);*/
+    })
+    .catch((err)=>{
+        res.status(400).send(err);
+    })
+};
+
+exports.read_deviceByDeviceDateTime= function(req, res) {
+    let device = req.query['idDevice'];
+    let date = req.query['date'];
+    let time = req.query['time'];
+
+    crate.execute("select * from etdevice where entity_id=? and date_format(time_index) like ? order by date_format(time_index) desc limit 1",[device, date+'T'+time+'%'])
+    .then((result) => {
+        console.log('Success', result.json, result.rowcount)
+        entidad = result.json;
+        /*let time_index = entidad[0]['time_index'];
+        construct a moment object with UTC-based input
+        var m = moment.tz(time_index,'America/Mexico_City');
+        format output however you desire
+        var dateFormated = m.format();
+        console.log(dateFormated);
+        entidad[0]['time_index'] = dateFormated;*/
+        res.status(200).json(entidad);
     })
     .catch((err)=>{
         res.status(400).send(err);
